6a3f387652a8584cefa4843636e7b355610b8239
Benjamin Holmes <benjamin.holmes@gmail.com>
2016-04-25 15:06:15 +0100

    Fixed dependency issues with Tests

--- src/test/java/com/redhat/bh/pt/gcal/CalendarAuthTest.java
+++ src/test/java/com/redhat/bh/pt/gcal/CalendarAuthTest.java
@@ -18,12 +18,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
